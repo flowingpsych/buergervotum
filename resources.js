bd1021460fd13d660d0bf9f4e8894b4bce7dc646
akless <andre.kless@web.de>
2020-09-29 11:55:35 +0200

    added app configuration

--- resources.js
+++ resources.js
@@ -5,6 +5,12 @@
 
 ccm.files[ 'resources.js' ] = {
 
+  "buergervotum": {
+    "font": [ "ccm.load", { "url": "https://fonts.googleapis.com/css?family=Courgette", "context": "head", "type": "css" } ],
+    "store": [ "ccm.store", { "url": "https://ccm2.inf.h-brs.de", "name": "quick_question" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.7.0.js", { "guest": true, "logged_in": true } ]
+  },
+
   "dashboard": {
     "data": {
       "store": [ "ccm.store", { "name": "quick_question", "url": "https://ccm2.inf.h-brs.de" } ],
